63ace6857c64b9cc67fabc033b5e72474100300d
dtruel <dtruel@users.noreply.github.com>
2016-12-30 18:24:07 -0600

    create setup.sh

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#run as root
+#for ubuntu 16.04
+
+apt-get update
+
+#install curl
+apt-get install curl
+
+#install git
+apt-get install git
+
+#install node version manager
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+
+#install node lts
+nvm install --lts
+
+#install mongodb
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+cat <<EOF >/lib/systemd/system/mongod.service
+[Unit]
+Description=High-performance, schema-free document-oriented database
+After=network.target
+Documentation=https://docs.mongodb.org/manual
+
+[Service]
+User=mongodb
+Group=mongodb
+ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable mongodb
+
+echo "done... probably want to run reboot"
